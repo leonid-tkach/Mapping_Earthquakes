160458d19f59a1cb7c6317a71df01806bd5dbd74
leonid-tkach <79228337+leonid-tkach@users.noreply.github.com>
2022-07-23 15:25:11 -0400

    13.6.2 fin

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -26,10 +26,33 @@ let map = L.map("mapid", {
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   console.log(data);
+  // This function determines the radius of the earthquake marker based on its magnitude.
+  // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  }
+  // This function returns the style data for each of the earthquakes we plot on
+  // the map. We pass the magnitude of the earthquake into a function
+  // to calculate the radius.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: "#ffae42",
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
   L.geoJSON(data, {
     pointToLayer: function(feature, latlng) {
       return L.circleMarker(latlng);
-    }
+    },
+    style: styleInfo
   }).addTo(map);
 });
 
